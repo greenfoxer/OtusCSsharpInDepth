66c781747ac8004cdbacc465aa1c0d29004d7799
Roman <greenfoxer@list.ru>
2025-11-19 15:32:38 +0300

    Домашнее задание 3

--- src/cache.SimpleStore/SimpleStore.cs
+++ src/cache.SimpleStore/SimpleStore.cs
@@ -6,26 +6,59 @@ using System.Threading.Tasks;
 
 namespace cache.SimpleStoreSystem
 {
-    public class SimpleStore
+    public class SimpleStore : IDisposable
     {
         private readonly Dictionary<string, byte[]> _cache;
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private long _setCount, _getCount, _deleteCount;
 
         public SimpleStore()
         {
             _cache = new Dictionary<string, byte[]>();
+            _setCount = 0;
+            _getCount = 0;
+            _deleteCount = 0;
         }
-
+        public (long SetCount,long GetCount,long DeleteCount) GetStatistics()
+        {
+            long sets = Interlocked.Read(ref _setCount);
+            long gets = Interlocked.Read(ref _getCount);
+            long deletes = Interlocked.Read(ref _deleteCount);
+            return (sets, gets, deletes);
+        }
         public void Set(string key, byte[] value)
         {
             if (!string.IsNullOrEmpty(key))
             {
-                _cache[key] = value;
+                try
+                {
+                    _lock.EnterWriteLock();
+                    _cache[key] = value;
+                    Interlocked.Increment(ref _setCount);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
             }
         }
         public byte[]? Get(string key) 
         {
-            if(_cache.TryGetValue(key, out var value))
-                return value;
+            try
+            {
+                _lock.EnterReadLock();
+
+                if (_cache.TryGetValue(key, out var value))
+                {
+                    Interlocked.Increment(ref _getCount);
+                    return value;
+                }
+
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
 
             return null;
         }
@@ -33,8 +66,41 @@ namespace cache.SimpleStoreSystem
         {
             if (!string.IsNullOrEmpty(key))
             {
-                _cache.Remove(key);
+                try
+                {
+                    _lock.EnterWriteLock();
+                    _cache.Remove(key);
+                    Interlocked.Increment(ref _deleteCount);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
             }
         }
+        #region IDisposable
+        private bool _disposed = false;
+        ~SimpleStore() 
+        {
+            Dispose(false);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _lock.Dispose();
+                    _cache.Clear();
+                }
+                _disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
---
+++ tests/cache.UnitTests/HomeWork2.cs
@@ -0,0 +1,68 @@
+﻿using cache.SimpleStoreSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cache.UnitTests
+{
+    public class HomeWork2 : IAsyncLifetime
+    {
+        public Task DisposeAsync()
+        {
+            _store.Dispose();
+            return Task.CompletedTask;
+        }
+        private SimpleStore _store;
+        public Task InitializeAsync()
+        {
+            _store = new SimpleStore();
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task SimpleStoreShouldMakeCorrectSetOperationStatisticsWithParallelWork()
+        {
+            var workTasks = new List<Task>();
+            var iterations = 1000;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                workTasks.Add(Task.Run(() => RunAddRequest(i)));
+            }
+            for (int i = 0; i < iterations; i++)
+            {
+                workTasks.Add(Task.Run(() => RunGetRequest(i)));
+            }
+            for (int i = 0; i < iterations; i++)
+            {
+                workTasks.Add(Task.Run(() => RunDelRequest(i)));
+            }
+
+            await Task.WhenAll(workTasks);
+            
+            var statistic = _store.GetStatistics();
+            Assert.Equal(iterations, statistic.SetCount);
+            Assert.Equal(iterations, statistic.GetCount);
+            Assert.Equal(iterations, statistic.DeleteCount);
+        }
+        private byte[] _testPrefix = { 0xD, 0xE, 0xA, 0xD, 0xB, 0xE, 0xE, 0xF };
+        private void RunAddRequest(int iteration)
+        {
+            var key = $"user:{iteration}";
+            var value = _testPrefix.Concat(BitConverter.GetBytes(iteration)).ToArray();
+            _store.Set(key, value);
+        }
+        private void RunGetRequest(int iteration)
+        {
+            var key = $"user:{iteration}";
+            _ = _store.Get(key);
+        }
+        private void RunDelRequest(int iteration)
+        {
+            var key = $"user:{iteration}";
+            _store.Delete(key);
+        }
+    }
+}
